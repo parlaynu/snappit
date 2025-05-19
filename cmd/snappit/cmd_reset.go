c65e9898171a4fe1bb9265019ac26fe988a6ace7
Paul Ryan <bngv8rqg@duck.com>
2025-05-20 07:18:47 +1000

    minor update to summary reporting

--- cmd/snappit/cmd_reset.go
+++ cmd/snappit/cmd_reset.go
@@ -28,27 +28,32 @@ func ResetSnapshot(config *Config, name string) error {
 	}
 
 	for _, archive := range config.Archives {
-		err = reset_archive(snapshot, archive.Label, archive.Source, config.SkipDirs)
+		fcount, err := reset_archive(snapshot, archive.Label, archive.Source, config.SkipDirs)
 		if err != nil {
 			return err
 		}
+		dcount := 0
 		if config.Prune {
-			err = prune_source(archive.Source, config.SkipDirs)
+			dcount, err = prune_source(archive.Source, config.SkipDirs)
 			if err != nil {
 				return err
 			}
+		}
+		fmt.Printf("checked %d files\n", fcount)
+		if config.Prune {
+			fmt.Printf("pruned %d directories\n", dcount)
 		}
 	}
 
 	return nil
 }
 
-func reset_archive(snapshot arena.Snapshot, label, source string, skipdirs []string,) error {
+func reset_archive(snapshot arena.Snapshot, label, source string, skipdirs []string,) (int, error) {
 	fmt.Printf("Resetting %s:%s\n", label, source)
 
 	archive, err := snapshot.LoadArchive(label)
 	if err != nil {
-		return err
+		return 0, err
 	}
 
 	// create the start channel - an early return will
@@ -59,26 +64,26 @@ func reset_archive(snapshot arena.Snapshot, label, source string, skipdirs []str
 	// build the pipeline of operators
 	ch1, err := ops.NewFsScanner(start, source, skipdirs)
 	if err != nil {
-		return err
+		return 0, err
 	}
 	ch1, err = ops.NewHashGenerator(ch1)
 	if err != nil {
-		return err
+		return 0, err
 	}
 
 	manifest := archive.Manifest()
 	ch2, err := ops.NewManifestReader(start, manifest, source)
 	if err != nil {
-		return err
+		return 0, err
 	}
 
 	ch, err := ops.NewStreamMerger(ch1, ch2)
 	if err != nil {
-		return err
+		return 0, err
 	}
 	ch, err = ops.NewFsReconciler(ch, source, archive.Data())
 	if err != nil {
-		return err
+		return 0, err
 	}
 
 	// and start the pipeline
@@ -95,13 +100,11 @@ func reset_archive(snapshot arena.Snapshot, label, source string, skipdirs []str
 			fmt.Printf("  %v\n", info.Error)
 		}
 	}
-	fmt.Printf("checked %d files\n", count)
 
-	return nil
+	return count, nil
 }
 
-func prune_source(source string, skipdirs []string) error {
-	fmt.Printf("Pruning %s\n", source)
+func prune_source(source string, skipdirs []string) (int, error) {
 
 	// create the start channel - an early return will
 	//  close the channel and shut everything down
@@ -110,15 +113,17 @@ func prune_source(source string, skipdirs []string) error {
 
 	ch, err := ops.NewFsPruner(start, source, skipdirs)
 	if err != nil {
-		return err
+		return 0, err
 	}
 	start <- true
 
+	count := 0
 	for info := range ch {
 		if info.Pruned {
+			count++
 			fmt.Printf("pruned %s\n", info.Directory)
 		}
 	}
 
-	return nil
+	return count, nil
 }
